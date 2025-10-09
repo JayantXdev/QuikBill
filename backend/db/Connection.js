27504db1c4161bebaacd343c0884eafc886c9846
JayantXdev <jayantarora123a@gmail.com>
2025-10-09 22:32:51 +0530

    first commit

--- backend/db/Connection.js
+++ backend/db/Connection.js
@@ -3,5 +3,7 @@ const mongoose=require("mongoose")
 const Connection=async()=>{
    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
    return console.log("Connected to MongoDB");
+//hhhh
+
 }
 module.exports=Connection
