7701439206e0ef14d062d473fb35ca7bd7ac4295
Anshbudhiraja <anshbudhiraja77@gmail.com>
2025-10-09 22:05:24 +0530

    First commit

--- backend/db/Connection.js
+++ backend/db/Connection.js
@@ -1,7 +1,12 @@
 const mongoose=require("mongoose")
 
 const Connection=async()=>{
-   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING)
    return console.log("Connected to MongoDB");
+  } catch (error) {
+   console.log("Error");
+   
+  }
 }
 module.exports=Connection
